9377fbb45736df8d2a7aee408e3b187f28674e1d
MalteWegener99 <malte.wegener99@gmx.de>
2018-12-17 20:13:51 +0100

    improved stuff and made more legible

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ use std::io::prelude::*;
 use std::fmt;
 use std::io::LineWriter;
 use std::io;
+use std::string;
 
 fn chord(y: f64) -> f64{
     if y<=12.2{
@@ -21,6 +22,140 @@ fn chord(y: f64) -> f64{
     }
     else{
         return 10.02-(10.02-5.52)/(32.5-12.2)*(y-12.2);
+    }
+}
+
+fn analyse(file: &str, minstr: u16) -> Vec<([f64; 4], [u16; 2], [f64; 4], [f64; 4])>{
+    let d: Vec<Dataset> = parse(file)
+                            .into_iter()
+                            .filter(|&thing| thing.shear != 0.)
+                            .collect::<Vec<Dataset> >();
+    let now = std::time::Instant::now();
+    let mut counter: u32 = 0;
+    let mut weight = 0.;
+
+    let mut results: Vec<([f64; 4], [u16; 2], [f64; 4], [f64; 4])> = Vec::new();
+    println!("{:?}", d);
+    for thing in &d{
+        counter += 1;
+        println!("{}", counter);
+        let vertices = [Vec2{x: 0., y: 0.}.scale(chord(thing.y)),Vec2{x: 0.45, y: 0.0182}.scale(chord(thing.y)),Vec2{x: 0.45, y: 0.1051}.scale(chord(thing.y)),Vec2{x: 0., y: 0.1092}.scale(chord(thing.y))];
+        let section = optimize(thing.torsion, thing.shear, thing.moment, 0.00001, vertices, 30).generateanalysable();
+        let maxes = section.get_max_stress(thing.torsion, thing.shear, thing.moment);
+        for i in 0..4{
+            let mut name = "";
+            match i{
+                0 => name = "Lower skin",
+                1 => name = "Rear spar",
+                2 => name = "Upper skin",
+                3 => name = "Front Spar",
+                _ => name = "Index out of bound error"
+            }
+            println!("{}: \t tau.max: {:.1} MPa, \t sigma.max {:.1} MPa", name, maxes.0[i]/1000000., maxes.1[i]/1000000.);
+        }
+        weight += section.get_weight_per_len(2700.)*0.1625;
+
+        results.push((section.skin_thicknesses,section.stringers, maxes.0, maxes.1));
+
+        println!("Mass per len:{:.2}", section.get_weight_per_len(2700.));
+        println!("{:.2}, {:.2}, {:.2}, {:.2}", section.skin_thicknesses[0]*1000.,section.skin_thicknesses[1]*1000.,section.skin_thicknesses[2]*1000.,section.skin_thicknesses[3]*1000.);
+        println!("{}, {}", section.stringers[0], section.stringers[1])
+    }
+    println!("I took {} seconds", (now.elapsed().as_secs() as f64));
+    println!("And i weigh {} kg", weight);
+    results
+}
+
+trait discretize{
+    fn discretize(&self, sections: &Vec<f64>) -> Vec<(f64, [f64; 4], [u16; 2])>;
+}
+
+trait save{
+    fn save(&self, file: &str) -> Result<(),io::Error>;
+}
+
+impl save for Vec<(f64, [f64; 4], [u16; 2])>{
+    fn save(&self, file: &str) -> Result<(),io::Error>{
+        let mut output = File::create(file)?;
+        for thing in self{
+            output.write_all(format!("{},{},{},{},{},{},{}\r\n", thing.0, thing.1[0], thing.1[1], thing.1[2], thing.1[3], thing.2[0], thing.2[1]).as_bytes())?;
+        }
+
+        Ok(())
+    }
+}
+
+impl discretize for Vec<([f64; 4], [u16; 2], [f64; 4], [f64; 4])>{
+    fn discretize(&self, sections: &Vec<f64>) -> Vec<(f64, [f64; 4], [u16; 2])>{
+        let lowersurface: Vec<f64> = self.into_iter().map(|x| x.0[0]).collect();
+        let rearspar: Vec<f64> = self.into_iter().map(|x| x.0[1]).collect();
+        let uppersurface: Vec<f64> = self.into_iter().map(|x| x.0[2]).collect();
+        let frontspar: Vec<f64> = self.into_iter().map(|x| x.0[3]).collect();
+
+        let lowerstr: Vec<u16> = self.into_iter().map(|x| x.1[0]).collect();
+        let upperstr: Vec<u16> = self.into_iter().map(|x| x.1[1]).collect();
+
+        //now lets find the niggershit section seperators
+        let dy = 0.1625;
+        let splits: Vec<usize> = sections.into_iter()
+                                            .map(|x| (x/dy) as usize)
+                                            .collect::<Vec<usize>>();
+
+
+        //Ok this is less rustic now. Man i fucking love functional programming
+        let mut lowersurfacemax: Vec<f64> = Vec::with_capacity(splits.len()+1).into_iter().map(|x: f64| 0.).collect();
+        let mut rearsparmax: Vec<f64> = Vec::with_capacity(splits.len()+1).into_iter().map(|x: f64| 0.).collect();
+        let mut uppersurfacemax: Vec<f64> = Vec::with_capacity(splits.len()+1).into_iter().map(|x: f64| 0.).collect();
+        let mut frontsparmax: Vec<f64> = Vec::with_capacity(splits.len()+1).into_iter().map(|x: f64| 0.).collect();
+
+        let mut lowerstrmax: Vec<u16> = Vec::with_capacity(splits.len()+1).into_iter().map(|x: u16| 0).collect();
+        let mut upperstrmax: Vec<u16> = Vec::with_capacity(splits.len()+1).into_iter().map(|x: u16| 0).collect();
+        for i in 0..splits.len()+1{
+            lowersurfacemax.push(0.);
+            rearsparmax.push(0.);
+            uppersurfacemax.push(0.);
+            frontsparmax.push(0.);
+
+            lowerstrmax.push(0);
+            upperstrmax.push(0);
+        }
+
+        for i in 0..lowersurface.len(){
+            for j in 0..splits.len(){
+                if i <= splits[j]{
+                    lowersurfacemax[j] = lowersurfacemax[j].max(lowersurface[i]);
+                    rearsparmax[j] = rearsparmax[j].max(rearspar[i]);
+                    uppersurfacemax[j] = uppersurfacemax[j].max(uppersurface[i]);
+                    frontsparmax[j] = frontsparmax[j].max(frontspar[i]);
+
+                    lowerstrmax[j] = lowerstrmax[j].max(lowerstr[i]);
+                    upperstrmax[j] = upperstrmax[j].max(upperstr[i]);
+                    break;
+                }
+            }
+            if i > splits[splits.len()-1]{
+                lowersurfacemax[splits.len()] = lowersurfacemax[splits.len()].max(lowersurface[i]);
+                rearsparmax[splits.len()] = rearsparmax[splits.len()].max(rearspar[i]);
+                uppersurfacemax[splits.len()] = uppersurfacemax[splits.len()].max(uppersurface[i]);
+                frontsparmax[splits.len()] = frontsparmax[splits.len()].max(frontspar[i]);
+
+                lowerstrmax[splits.len()] = lowerstrmax[splits.len()].max(lowerstr[i]);
+                upperstrmax[splits.len()] = upperstrmax[splits.len()].max(upperstr[i]);
+            }
+        }
+
+        let mut ret = Vec::with_capacity(lowersurfacemax.len());
+
+        for i in 0..lowersurfacemax.len(){
+            let y =  if i > 0 {
+                sections[i-1]
+            }else{ 
+                0.
+            };
+            ret.push((y, [lowersurfacemax[i],rearsparmax[i],uppersurfacemax[i],frontsparmax[i]], [lowerstrmax[i],upperstrmax[i]]));
+        }
+
+        ret
     }
 }
 
@@ -37,111 +172,8 @@ fn main() {
         Ok(i) => println!("Good input"),
         Err(..) => println!("this was not an integer: {}", trimmed),
     };
-    let stringersinput = trimmed.parse::<u32>().unwrap();
+    let stringersinput = trimmed.parse::<u16>().unwrap();
 
-    let d: Vec<Dataset> = parse("output.csv")
-                            .into_iter()
-                            .filter(|&thing| thing.shear != 0.)
-                            .collect::<Vec<Dataset> >();
-    let now = std::time::Instant::now();
-    let mut counter: u32 = 0;
-    let mut weight = 0.;
-
-    let mut results: Vec<([f64; 4], [u16; 2], [f64; 4], [f64; 4])> = Vec::new();
-    println!("{:?}", d);
-    for thing in &d{
-        counter += 1;
-        println!("{}", counter);
-        let vertices = [Vec2{x: 0., y: 0.}.scale(chord(thing.y)),Vec2{x: 0.45, y: 0.0182}.scale(chord(thing.y)),Vec2{x: 0.45, y: 0.1051}.scale(chord(thing.y)),Vec2{x: 0., y: 0.1092}.scale(chord(thing.y))];
-        let section = optimize(thing.torsion, thing.shear, thing.moment, 0.00001, vertices, 30).generateanalysable();
-        let maxes = section.get_max_stress(thing.torsion, thing.shear, thing.moment);
-        for i in 0..4{
-            let mut name = "";
-            match i{
-                0 => name = "Lower skin",
-                1 => name = "Rear spar",
-                2 => name = "Upper skin",
-                3 => name = "Front Spar",
-                _ => name = "Index out of bound error"
-            }
-            println!("{}: \t tau.max: {:.1} MPa, \t sigma.max {:.1} MPa", name, maxes.0[i]/1000000., maxes.1[i]/1000000.);
-        }
-        weight += section.get_weight_per_len(2700.)*0.1625;
-
-        results.push((section.skin_thicknesses,section.stringers, maxes.0, maxes.1));
-
-        println!("Mass per len:{:.2}", section.get_weight_per_len(2700.));
-        println!("{:.2}, {:.2}, {:.2}, {:.2}", section.skin_thicknesses[0]*1000.,section.skin_thicknesses[1]*1000.,section.skin_thicknesses[2]*1000.,section.skin_thicknesses[3]*1000.);
-        println!("{}, {}", section.stringers[0], section.stringers[1])
-    }
-    println!("I took {} seconds", (now.elapsed().as_secs() as f64));
-    println!("And i weigh {} kg", weight);
-
-    let mut output = File::create("Design.csv");
-    let mut output = match output {
-        Ok(file) => file,
-        Err(error) => {
-            panic!("There was a problem opening the file: {:?}", error)
-        },
-    };
-
-    let mut output = LineWriter::new(output);
-    for res in &results{
-        output.write_all(format!("{},{},{},{},{},{},{},{},{},{},{},{},{},{}\r\n", res.0[0], res.0[1], res.0[2], res.0[3], res.1[0], res.1[1],
-                                                                                    res.2[0],res.2[1],res.2[2],res.2[3],res.3[0],res.3[1],res.3[2],res.3[3]).as_bytes());
-    }
-    println!("saved1");
-
-    let d = parse("output-1.csv")
-                            .into_iter()
-                            .filter(|&thing| thing.shear != 0.)
-                            .collect::<Vec<Dataset> >();
-    let now = std::time::Instant::now();
-    let mut counter: u32 = 0;
-    let mut weight = 0.;
-
-    let mut results: Vec<([f64; 4], [u16; 2], [f64; 4], [f64; 4])> = Vec::new();
-    println!("{:?}", d);
-    for thing in &d{
-        counter += 1;
-        println!("{}", counter);
-        let vertices = [Vec2{x: 0., y: 0.}.scale(chord(thing.y)),Vec2{x: 0.45, y: 0.0182}.scale(chord(thing.y)),Vec2{x: 0.45, y: 0.1051}.scale(chord(thing.y)),Vec2{x: 0., y: 0.1092}.scale(chord(thing.y))];
-        let section = optimize(thing.torsion, thing.shear, thing.moment, 0.00001, vertices, 30).generateanalysable();
-        let maxes = section.get_max_stress(thing.torsion, thing.shear, thing.moment);
-        for i in 0..4{
-            let mut name = "";
-            match i{
-                0 => name = "Lower skin",
-                1 => name = "Rear spar",
-                2 => name = "Upper skin",
-                3 => name = "Front Spar",
-                _ => name = "Index out of bound error"
-            }
-            println!("{}: \t tau.max: {:.1} MPa, \t sigma.max {:.1} MPa", name, maxes.0[i]/1000000., maxes.1[i]/1000000.);
-        }
-        weight += section.get_weight_per_len(2700.)*0.1625;
-
-        results.push((section.skin_thicknesses,section.stringers, maxes.0, maxes.1));
-
-        println!("Mass per len:{:.2}", section.get_weight_per_len(2700.));
-        println!("{:.2}, {:.2}, {:.2}, {:.2}", section.skin_thicknesses[0]*1000.,section.skin_thicknesses[1]*1000.,section.skin_thicknesses[2]*1000.,section.skin_thicknesses[3]*1000.);
-        println!("{}, {}", section.stringers[0], section.stringers[1])
-    }
-    println!("I took {} seconds", (now.elapsed().as_secs() as f64));
-    println!("And i weigh {} kg", weight);
-
-    let mut output = File::create("Design-1.csv");
-    let mut output = match output {
-        Ok(file) => file,
-        Err(error) => {
-            panic!("There was a problem opening the file: {:?}", error)
-        },
-    };
-
-    let mut output = LineWriter::new(output);
-    for res in &results{
-        output.write_all(format!("{},{},{},{},{},{},{},{},{},{},{},{},{},{}\r\n", res.0[0], res.0[1], res.0[2], res.0[3], res.1[0], res.1[1],
-                                                                                    res.2[0],res.2[1],res.2[2],res.2[3],res.3[0],res.3[1],res.3[2],res.3[3]).as_bytes());
-    }
-    println!("saved2")
+    analyse("output.csv", stringersinput).discretize(&vec![12.2, 20., 25., 30.]).save("Design.csv");
+    analyse("output-1.csv", stringersinput).discretize(&vec![12.2, 20., 25., 30.]).save("Design-1.csv");
 }
--- src/wing.rs
+++
@@ -1,6 +0,0 @@
-use Vector:Vec2;
-use section::{section_prototype, Analysable_section};
-
-struct Wing{
-    let sections: Vec<(f64, section_prototype)>;
-}
